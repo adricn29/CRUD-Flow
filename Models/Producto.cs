838bd2255caeb5e1d44facc5b2d6e0d6da94d8af
adricn29 <152666434+adricn29@users.noreply.github.com>
2025-04-02 19:46:05 -0400

    Delete Models directory

--- Models/Producto.cs
+++
@@ -1,9 +0,0 @@
-namespace CRUDApp.Models
-{
-    public class Producto
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public decimal Precio { get; set; }
-    }
-}
