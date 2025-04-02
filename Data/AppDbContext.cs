9653b750c474c90ef7d09b1ec74ed5b81ae2ec67
adricn29 <152666434+adricn29@users.noreply.github.com>
2025-04-02 19:21:13 -0400

    Create AppDbContext.cs

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using CRUDApp.Models;
+
+namespace CRUDApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
