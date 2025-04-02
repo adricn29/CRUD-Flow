ec35140a5ecde27e3098de657b1cc4f4a9d4164a
adricn29 <152666434+adricn29@users.noreply.github.com>
2025-04-02 19:33:53 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,37 @@
 using Microsoft.EntityFrameworkCore;
 using CRUDApp.Data;
 
-var builder = WebApplication.CreateBuilder(args);
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Agregar servicios al contenedor.
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+        builder.Services.AddControllersWithViews();
+
+        var app = builder.Build();
+
+        // Configurar el pipeline de la aplicación HTTP.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapDefaultControllerRoute();
+
+        app.Run();
+    }
+}
