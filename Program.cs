53ab28ccb863eea7d86c36c1e7d4350e11fad859
adricn29 <152666434+adricn29@users.noreply.github.com>
2025-04-02 19:27:08 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
+
+using CRUDApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Registrar AppDbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// El resto de la configuración
+
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
